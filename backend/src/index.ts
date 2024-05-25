a220c69850ec04ed035479f43dcf45fe9edd6bfb
NazmulHossainShovon <shovon2228@gmail.com>
2024-05-25 22:06:59 +0600

    issue #2: initial api

---
+++ backend/src/index.ts
@@ -0,0 +1,37 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import { userRouter } from "./routers/userRouter";
+
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch(() => {
+    console.log("error mongodb");
+  });
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173"],
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", userRouter);
+
+const PORT: number = parseInt((process.env.PORT || "4000") as string, 10);
+
+app.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`);
+});
