a3c60912a1b2ae813541c2ed8d638f3ae20a77b5
NazmulHossainShovon <shovon2228@gmail.com>
2024-06-17 23:30:59 +0600

    issue #6: add modal component

--- frontend/src/Pages/Home/index.tsx
+++ frontend/src/Pages/Home/index.tsx
@@ -1,12 +1,30 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Store } from '../../Store';
+import { Box, Button, Modal, TextField } from '@mui/material';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  height: 450,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function Home() {
   const navigate = useNavigate();
   const {
     state: { userInfo },
   } = useContext(Store);
+  const [post, setPost] = useState('');
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     const userJson = localStorage.getItem('user-info');
@@ -16,8 +34,29 @@ function Home() {
   }, [navigate]);
 
   return (
-    <div className=" align-middle text-center text-lg font-bold text-blue-600">
-      Hi {userInfo?.name}
+    <div className="flex flex-col gap-3 items-center align-middle ">
+      <h1 className="text-center text-lg font-bold text-blue-600">
+        Hi {userInfo?.name}
+      </h1>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <TextField
+            onChange={e => setPost(e.target.value)}
+            label="Create Post"
+            multiline
+          />
+          <Button onClick={() => console.log('post')}>Post</Button>
+        </Box>
+      </Modal>
+      <Button onClick={handleOpen} variant="outlined">
+        Whats on your mind?
+      </Button>
     </div>
   );
 }
