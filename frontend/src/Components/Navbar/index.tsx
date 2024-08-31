e91bc6a1b4b757e98262662fbefd972a277f268d
NazmulHossainShovon <shovon2228@gmail.com>
2024-08-31 21:59:09 +0600

    issue #23: navbar responsive

--- frontend/src/Components/Navbar/index.tsx
+++ frontend/src/Components/Navbar/index.tsx
@@ -4,7 +4,9 @@ import { useContext, useState } from 'react';
 import { Store } from '../../Store';
 import Input from '../Input';
 import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
 import FriendReqsMenu from '../FriendReqsMenu';
+import { Box, Button, Drawer } from '@mui/material';
 
 function Navbar() {
   const {
@@ -12,18 +14,23 @@ function Navbar() {
   } = useContext(Store);
   const [searchQuery, setSearchQuery] = useState('');
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
 
   const handleSearchQuery = () => {
     navigate(`/search/${searchQuery}`);
   };
 
   return (
-    <div className="flex justify-start pl-5 items-center gap-5 bg-TK-background   text-white h-[60px]">
+    <div className="flex justify-between md:justify-start pl-5 items-center gap-5 bg-TK-background  text-white h-[60px]">
       {userInfo ? (
         <>
           <Link to={'/'}>Home</Link>
           <AvatarMenu />
-          <div className="flex flex-row ml-5 items-center gap-2">
+          <div className="hidden md:flex  flex-row ml-5 items-center gap-2">
             <Input
               placeholder="Search anything..."
               onChange={e => setSearchQuery(e.target.value)}
@@ -34,6 +41,25 @@ function Navbar() {
             />
           </div>
           <FriendReqsMenu />
+
+          <MenuIcon className="block md:hidden" onClick={toggleDrawer(true)} />
+          <Drawer open={open} onClose={toggleDrawer(false)}>
+            <Box
+              sx={{ width: 250, paddingLeft: '10px', paddingTop: '10px' }}
+              role="presentation"
+            >
+              <div className="flex m-3 flex-row ml-5 items-center gap-2">
+                <Input
+                  placeholder="Search anything..."
+                  onChange={e => setSearchQuery(e.target.value)}
+                />
+                <SearchIcon
+                  onClick={handleSearchQuery}
+                  className=" cursor-pointer"
+                />
+              </div>
+            </Box>
+          </Drawer>
         </>
       ) : (
         <>
