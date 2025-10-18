a84976d7fbeb7019fe06cd6f30cf2f61f653446d
NazmulHossainShovon <shovon2228@gmail.com>
2025-10-18 14:01:48 +0600

    terms page

---
+++ nextapp/app/terms-and-conditions/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from '../../components/Navbar';
+
+export default function TermsAndConditionsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        {children}
+      </main>
+    </div>
+  );
+}
