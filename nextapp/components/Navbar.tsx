4cea6a272b774f71696e9d96744ee9f821c7cacc
NazmulHossainShovon <shovon2228@gmail.com>
2025-10-18 10:31:26 +0600

    main navbar logout

--- nextapp/components/Navbar.tsx
+++ nextapp/components/Navbar.tsx
@@ -1,6 +1,19 @@
+'use client';
+
 import Link from 'next/link';
+import { useContext } from 'react';
+import { Store } from '../app/lib/store';
+import { handleLogout } from '../app/utils/logout';
+import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
+  const { state: { userInfo }, dispatch } = useContext(Store);
+  const router = useRouter();
+
+  const handleUserLogout = () => {
+    handleLogout(dispatch, router);
+  };
+
   return (
     <nav className="bg-black p-4">
       <ul className="flex space-x-4">
@@ -9,16 +22,29 @@ export default function Navbar() {
             All apps
           </Link>
         </li>
-        <li>
-          <Link href="/signup" className="text-white hover:text-gray-300">
-            Sign Up
-          </Link>
-        </li>
-        <li>
-          <Link href="/login" className="text-white hover:text-gray-300">
-            Login
-          </Link>
-        </li>
+        {!userInfo?.name ? (
+          <>
+            <li>
+              <Link href="/signup" className="text-white hover:text-gray-300">
+                Sign Up
+              </Link>
+            </li>
+            <li>
+              <Link href="/login" className="text-white hover:text-gray-300">
+                Login
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li>
+            <button 
+              onClick={handleUserLogout}
+              className="text-white hover:text-gray-300 cursor-pointer"
+            >
+              Logout
+            </button>
+          </li>
+        )}
       </ul>
     </nav>
   );
